67d4b0a8668df81a838c2072785a15adb46dd44c
mtobdvlb <mtobdvlb@163.com>
2025-03-27 08:58:49 +0800

    配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
+// vite.config.ts
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
+import {viteMockServe} from 'vite-plugin-mock'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+
+
+    plugins: [vue(),
+        createSvgIconsPlugin({
+          iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+          symbolId: 'icon-[dir]-[name]',
+        }),
+        viteMockServe({
+          enable: true,
+        })
+      ],
+    resolve: {
+      alias: {
+          "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
+      }
+    },
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData:  `@use "@/styles/variable.scss" as *;`,
+          },
+        },
+      },
+      
+
 })
